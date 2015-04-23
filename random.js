434bcac53db0c521fd2895aa7fd4d407e3bb1426
mariamtm <meryalca@gmail.com>
2015-04-23 14:31:50 +0200

    random version 2

--- random.js
+++ random.js
@@ -1,6 +1,10 @@
 // Math.random() devuelve número aleatorio entre 0 y 1.
 var numero = Math.random();
 
-if (humero <= 0.5){
-  console.log
+var str = 'MAYOR que 0.5';
+
+if(numero <= 0.5){
+  str = 'MENOR que 0.5';
 }
+
+console.log('\n' + numero + str + '\n');
